66246c0cb75a1aefa170a592d9e66dbd1c218cf5
Null | Void <36554880+VoidNulll@users.noreply.github.com>
2018-09-29 15:48:51 -0500

    Updated how assets are handled

--- profiles/chilling.js
+++ profiles/chilling.js
@@ -1,11 +1,16 @@
 module.exports = () =>{
     return {
-        bigFileIcon: 'chilling',
-        bigIconTxt: 'Chilling',
-        littleFileIcon: 'ice',
-        littleIconTxt: 'Ice ice baby',
-        state: `Chilling`,
-        details: 'Chilling',
-        name: 'chilling'
+        assets: {
+            small: {
+                txt: 'Cold hearted', //The text you get when hovering your mouse over the small icon
+                icon: 'ice' //The small icon in the bottom right
+            }, large: {
+                txt: 'Oh hey, some trees!', //The text that appears after hovering your mouse over the large icdon
+                icon: 'trees' //The large icon for the rich presence
+            }
+        },
+        details: 'Im just chillin\'', //The details of the rich presence
+        state: 'Chillin\'', //The state of the rich presence
+        name: 'chilling' //The name of the profile
     }
 }
